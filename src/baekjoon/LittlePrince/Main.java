c88cb811e2fad2a970217dfb35c5c61016e6024d
jeongwle <dldl4568@naver.com>
2022-09-03 16:15:20 +0900

    [Feat] baekjoon 1004 LittlePrince first - 출발점과 도착점을 둘러싸고 있는 원의 개수를 구하면 탈출 혹은 진입을 계산할 수 있다 생각함 - 그렇게 첫 번째 코드를 제출했으나 틀렸음.

---
+++ src/baekjoon/LittlePrince/Main.java
@@ -0,0 +1,68 @@
+package baekjoon.LittlePrince;
+
+/*
+ 출발점과 도착점을 둘러싸고 있는 원의 개수를 구해야 한다고 생각을 하였음.
+ */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final int CASE = Integer.parseInt(scanner.nextLine());
+        String[] input;
+
+        for (int i = 0; i < CASE; i++) {
+            int result = 0;
+            input = scanner.nextLine().split(" ");
+            Point depart = new Point(Integer.parseInt(input[0]), Integer.parseInt(input[1]));
+            Point arrival = new Point(Integer.parseInt(input[2]), Integer.parseInt(input[3]));
+
+            final int PLANET_COUNT = Integer.parseInt(scanner.nextLine());
+            Planet[] planets = new Planet[PLANET_COUNT];
+            for (int j = 0; j < PLANET_COUNT; j++) {
+                input = scanner.nextLine().split(" ");
+                planets[j] = new Planet(
+                        new Point(Integer.parseInt(input[0]), Integer.parseInt(input[1])), Integer.parseInt(input[2])
+                );
+                if (planets[j].isInside(depart)) {
+                    ++result;
+                }
+                if (planets[j].isInside(arrival)) {
+                    ++result;
+                }
+            }
+            System.out.println(result);
+        }
+    }
+}
+
+class Planet {
+    Point center;
+    int radius;
+
+    Planet(Point p, int radius) {
+        this.center = p;
+        this.radius = radius;
+    }
+
+    int getDistance(Point target) {
+        return (int) Math.sqrt(
+                Math.pow(this.center.x - target.x, 2) + Math.pow(this.center.y - target.y, 2)
+        );
+    }
+
+    boolean isInside(Point target) {
+        return this.radius > this.getDistance(target);
+    }
+}
+
+class Point {
+    int x;
+    int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
