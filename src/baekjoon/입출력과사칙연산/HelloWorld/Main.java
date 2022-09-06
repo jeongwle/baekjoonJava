6e9fbe249d53521c585051fbd446bd4065cf6688
jeongwle <dldl4568@naver.com>
2022-09-06 19:07:13 +0900

    [Feat] baekjoon 1단계 입출력과 사칙연산 - 대부분 단계별로 풀어보기를 하는 것 같아서 나도 단계별로 풀어보기를 하려고 한다. - 일단 이번 1단계는 너무 쉬웠다. 하지만 그래도 자바에 익숙해지기 위해서 그리고   혹시 모르는 것들이 나올 수도 있으니 차근차근 하나씩 해보자.

---
+++ src/baekjoon/입출력과사칙연산/HelloWorld/Main.java
@@ -0,0 +1,7 @@
+package baekjoon.입출력과사칙연산.HelloWorld;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/baekjoon/입출력과사칙연산/사칙연산/Main.java
@@ -0,0 +1,11 @@
+package baekjoon.입출력과사칙연산.사칙연산;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(scanner.nextLine() + "??!");
+    }
+}
