34537fa1a989a266ebe65cee15fcbd72513801a5
jeongwle <dldl4568@naver.com>
2022-09-17 16:50:52 +0900

    [Refactor] baekjoon 7단계 기본수학 1  - 기존의 코드는 sales로 나누기 때문에 sales가 0일 경우를 처리해주어야 했는데    반복문의 조건문을 바꿈으로써 sales가 0일때를 체크하는 코드가 불필요해졌다.

--- src/baekjoon/기본수학1/손익분기점/Main.java
+++ src/baekjoon/기본수학1/손익분기점/Main.java
@@ -41,16 +41,19 @@ class BreakEvenPoint {
         if (NOTEBOOK_PRICE <= VARIABLE_COST) {
             return -1;
         }
-        int sales = FIXED_COST / (NOTEBOOK_PRICE - VARIABLE_COST); // 판매량은 고정비용을 (노트북가격 - 가변비용)으로 나눈것보다 커야한다.
-        // 나눴을 때 0인 경우 1부터 탐색하도록 해야한다.
-        if (sales == 0) {
-            sales = 1;
-        }
-        // 손익분기점 구하기
-        while (NOTEBOOK_PRICE <= (FIXED_COST / sales) + VARIABLE_COST) {
+        /*
+         판매량은 고정비용을 (노트북가격 - 가변비용)으로 나눈것보다 커야한다.
+         그래서 sales 판매량의 시작기준을 고정비용 / (노트북 가격 - 가변비용)으로 한다.
+         */
+        int sales = FIXED_COST / (NOTEBOOK_PRICE - VARIABLE_COST);
+        /*
+         손익분기점 구하기 기존 sales로 나누던 코드 때문에 sales가 0일때를 체크하는 코드를 사용했었는데
+         조건문을 바꿈으로써 쓸모없는 코드를 줄일 수 있는 것 같아서 바꾸어 보았다.
+         */
+
+        while (sales <= FIXED_COST / (NOTEBOOK_PRICE - VARIABLE_COST)) {
             sales++;
         }
         return sales;
     }
-
 }
