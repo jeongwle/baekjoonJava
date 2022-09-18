b1f80c90c3b7242fe3d25fbfa2a38a566e0d2bfb
jeongwle <dldl4568@naver.com>
2022-09-18 19:15:07 +0900

    [Modify] baekjoon 7단계 기본수학 4  - Scanner를 빼고 BufferedReader를 사용했다.

--- src/baekjoon/기본수학1/달팽이는올라가고싶다/Main2.java
+++ src/baekjoon/기본수학1/달팽이는올라가고싶다/Main2.java
@@ -3,18 +3,22 @@ package baekjoon.기본수학1.달팽이는올라가고싶다;
 백준 2869 시간초를 위해 짜본 코드
  */
 
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main2 {
-    public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        final int CLIMBING_HEIGHT = Integer.parseInt(st.nextToken());
+        final int FALLING_HEIGHT = Integer.parseInt(st.nextToken());
+        final int TARGET_HEIGHT = Integer.parseInt(st.nextToken());
 
-        final int CLIMBING_HEIGHT = scanner.nextInt();
-        final int FALLING_HEIGHT = scanner.nextInt();
-        final int TARGET_HEIGHT = scanner.nextInt();
-
-        int day = (int)Math.ceil(
-                (double)(TARGET_HEIGHT - FALLING_HEIGHT) / (CLIMBING_HEIGHT - FALLING_HEIGHT)
+        int day = (int) Math.ceil(
+                (double) (TARGET_HEIGHT - FALLING_HEIGHT) / (CLIMBING_HEIGHT - FALLING_HEIGHT)
         );
         System.out.println(day);
     }
