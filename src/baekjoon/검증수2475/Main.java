ff7d7a5edcb0fad874aec770159b418dd57320a6
jeongwle <dldl4568@naver.com>
2022-11-06 11:49:54 +0900

    [Feat] baekjoon 검증수 2475

---
+++ src/baekjoon/검증수2475/Main.java
@@ -0,0 +1,26 @@
+package baekjoon.검증수2475;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+
+        int sum = 0;
+        while (st.hasMoreTokens()) {
+            sum += Math.pow(toInt(st.nextToken()), 2);
+        }
+        System.out.println(sum % 10);
+
+    }
+
+    public static int toInt(String input) {
+        return Integer.parseInt(input);
+    }
+
+}
