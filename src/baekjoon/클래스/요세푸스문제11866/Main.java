4e6a1fb41412209fd13743f083f4a4672b41e222
jeongwle <dldl4568@naver.com>
2022-11-15 23:35:50 +0900

    feat : baekjoon 요세푸스 11866

---
+++ src/baekjoon/클래스/요세푸스문제11866/Main.java
@@ -0,0 +1,48 @@
+package baekjoon.클래스.요세푸스문제11866;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+
+        int N = toInt(st.nextToken());
+        int targetIndex = toInt(st.nextToken());
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            q.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+
+        int index = 1;
+        while (!q.isEmpty()) {
+            if (index == targetIndex) {
+                sb.append(q.poll());
+                if (!q.isEmpty()) {
+                    sb.append(", ");
+                }
+                index = 1;
+                continue;
+            } else {
+                q.add(q.poll());
+            }
+            index++;
+        }
+        sb.append(">");
+        System.out.println(sb);
+    }
+
+    public static int toInt(String input) {
+        return Integer.parseInt(input);
+    }
+}
