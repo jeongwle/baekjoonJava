25cd82caf6f922d3bc5f48407398c98f18093c83
jeongwle <dldl4568@naver.com>
2022-12-18 13:35:48 +0900

    feat : baekjoon 단어길이재기 2743
    무의미..하하

---
+++ src/baekjoon/기타/단어길이재기2743/Main.java
@@ -0,0 +1,16 @@
+package baekjoon.기타.단어길이재기2743;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = bf.readLine();
+        System.out.println(s.length());
+    }
+
+}
