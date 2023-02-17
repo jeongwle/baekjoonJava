1790485985f67adcf13b357f6ee260cd1726ff5b
jeongwle <dldl4568@naver.com>
2023-02-17 17:31:50 +0900

    feat: baekjoon 의미없는 문제

---
+++ src/baekjoon/기타/등록7287/Main.java
@@ -0,0 +1,9 @@
+package baekjoon.기타.등록7287;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("234");
+        System.out.println("111121121");
+    }
+}
