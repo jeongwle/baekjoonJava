3c2039ef55856a885fcfd334241c7c51fe9870ad
jeongwle <dldl4568@naver.com>
2023-02-19 21:23:51 +0900

    feat: bakjoon 대소문자바꾸기 2744

---
+++ src/baekjoon/기타/대소문자바꾸기2744/Main.java
@@ -0,0 +1,23 @@
+package baekjoon.기타.대소문자바꾸기2744;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = bf.readLine();
+        StringBuilder sb = new StringBuilder();
+        for (char c : input.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                sb.append(Character.toLowerCase(c));
+            } else {
+                sb.append(Character.toUpperCase(c));
+            }
+        }
+        System.out.println(sb);
+    }
+}
