578c3aae27c74eb1a9ec75861cd419c71152e55a
jeongwle <dldl4568@naver.com>
2023-03-04 00:21:45 +0900

    feat: bakjoon 별찍기 2444

---
+++ src/baekjoon/기타/별찍기2444/Main.java
@@ -0,0 +1,34 @@
+package baekjoon.기타.별찍기2444;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bf.readLine());
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 1; i <= N; i++) {
+            drawStar(N, answer, i);
+        }
+
+        for (int i = N - 1; i >= 1; i--) {
+            drawStar(N, answer, i);
+        }
+        System.out.print(answer);
+    }
+
+    private static void drawStar(int n, StringBuilder answer, int i) {
+        for (int j = 0; j < n - i; j++) {
+            answer.append(" ");
+        }
+        for (int j = 0; j < 2 * i - 1; j++) {
+            answer.append("*");
+        }
+        answer.append("\n");
+    }
+}
