bb45c4f590c09eac0091548c879c8d1d532124a3
jeongwle <dldl4568@naver.com>
2023-03-13 21:56:37 +0700

    feat: bakjoon APlusB 2558

---
+++ src/baekjoon/기타/APlusB2558/Main.java
@@ -0,0 +1,17 @@
+package baekjoon.기타.APlusB2558;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int A = Integer.parseInt(bf.readLine());
+        int B = Integer.parseInt(bf.readLine());
+        System.out.println(A + B);
+    }
+
+}
