27e2fc9e3b3d4e01f1c3f41b308d7dbc6e2dc6ba
jeongwle <dldl4568@naver.com>
2023-03-19 16:48:17 +0700

    feat: baekjoon 직사각형 27323

---
+++ src/baekjoon/기타/직사각형27323/Main.java
@@ -0,0 +1,13 @@
+package baekjoon.기타.직사각형27323;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Integer.parseInt(bf.readLine()) * Integer.parseInt(bf.readLine()));
+    }
+}
