6bcf80bfd5d5302f9b0a4468d7bb2a55da99b3f4
jeongwle <dldl4568@naver.com>
2023-03-20 20:53:59 +0700

    feat: baekjoon 수학은체육과목입니다 15894

---
+++ src/baekjoon/기타/수학은체육과목입니다15894/Main.java
@@ -0,0 +1,15 @@
+package baekjoon.기타.수학은체육과목입니다15894;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bf.readLine());
+        System.out.println(4L * N);
+    }
+}
