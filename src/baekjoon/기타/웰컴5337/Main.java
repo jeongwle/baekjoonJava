067deede47147901f2ef53e91f441a6372f85a2e
jeongwle <dldl4568@naver.com>
2023-03-24 23:53:07 +0700

    feat: baekjoon 웰컴 5337

---
+++ src/baekjoon/기타/웰컴5337/Main.java
@@ -0,0 +1,10 @@
+package baekjoon.기타.웰컴5337;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(".  .   .\n"
+                + "|  | _ | _. _ ._ _  _\n"
+                + "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
