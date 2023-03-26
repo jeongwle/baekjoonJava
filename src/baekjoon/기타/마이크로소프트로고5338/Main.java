2ec66518c699faac264583c4a56f4227414ffc22
jeongwle <dldl4568@naver.com>
2023-03-26 21:14:54 +0700

    feat: baekjoon 마이크로소프트로고 5338

---
+++ src/baekjoon/기타/마이크로소프트로고5338/Main.java
@@ -0,0 +1,12 @@
+package baekjoon.기타.마이크로소프트로고5338;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._\n"
+                + "'-..-'|   ||   |\n"
+                + "'-..-'|_.-;;-._|\n"
+                + "'-..-'|   ||   |\n"
+                + "'-..-'|_.-''-._|");
+    }
+}
