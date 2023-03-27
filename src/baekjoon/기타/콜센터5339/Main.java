5b941961808734fa8240852a3b602d32322091a8
jeongwle <dldl4568@naver.com>
2023-03-27 22:01:56 +0700

    feat: baekjoon 콜센터 5339

---
+++ src/baekjoon/기타/콜센터5339/Main.java
@@ -0,0 +1,17 @@
+package baekjoon.기타.콜센터5339;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("     /~\\\n"
+                + "    ( oo|\n"
+                + "    _\\=/_\n"
+                + "   /  _  \\\n"
+                + "  //|/.\\|\\\\\n"
+                + " ||  \\ /  ||\n"
+                + "============\n"
+                + "|          |\n"
+                + "|          |\n"
+                + "|          |");
+    }
+}
