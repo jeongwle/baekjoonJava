1fcc59f78a2ae14cbccdda74b48458967c9be77d
jeongwle <dldl4568@naver.com>
2023-04-08 21:43:15 +0700

    feat: baekjoon 16394

---
+++ src/baekjoon/기타/홍익대학교16394/Main.java
@@ -0,0 +1,12 @@
+package baekjoon.기타.홍익대학교16394;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int now = sc.nextInt();
+        System.out.println(now - 1946);
+    }
+}
