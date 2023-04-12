2bbc4067238821fc789967921e4085d96cc946a3
jeongwle <dldl4568@naver.com>
2023-04-12 21:58:40 +0700

    feat: baekjoon 10189

---
+++ src/baekjoon/기타/Hook10189/Main.java
@@ -0,0 +1,10 @@
+package baekjoon.기타.Hook10189;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #");
+    }
+}
